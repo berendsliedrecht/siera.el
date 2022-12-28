4e581c98c84d2de86357768a670e903833da8ce3
blu3beri <blu3beri@proton.me>
2022-12-28 16:21:15 +0100

    seperated string-join and added plist
    Signed-off-by: blu3beri <blu3beri@proton.me>

--- siera.el
+++ siera.el
@@ -1,9 +1,13 @@
+(defun string-join-with-delim (args &optional delim)
+  (or delim (setq b " "))
+  (string-join args " "))
+
 (defun siera--command (&rest command)
   "Wrapper for siera"
   (setq siera (executable-find "siera"))
   (if (not siera)
       (error "siera is not installed"))
-  (setq formatted-command (format "%s %s 2> /dev/null" siera (string-join command " "))
+  (setq formatted-command (format "%s %s 2> /dev/null" siera (string-join-with-delim command))
         command-output (shell-command-to-string formatted-command)
         out command-output)
   (message out))
@@ -11,10 +15,19 @@
 (defmacro siera! (&rest commands)
   (macroexp-progn
    (mapcar (lambda (command)
-             `(defun ,(intern (format "siera--%s" (string-join command "-"))) (&optional args)
+             `(defun ,(intern (format "siera--%s" (string-join-with-delim command "-"))) (&optional args)
                 ,(format "Siera wrapper for the %s subcommand" command)
-                (siera--command ,(string-join command " ") args))
+                (siera--command ,(string-join-with-delim command) args))
              ) commands)))
 
-(siera! ("connection" "invite"))
-(siera--connection-invite "--qr")
+(defun sample-siera (plist)
+  (setq commands (list :connections :proofs :credentials))
+  (dolist (command commands)
+    (setq subcommand (plist-get plist command))
+    (when (boundp 'subcommand)
+        (message subcommand))))
+
+(sample-siera '(:connections "invite"
+                :proofs "foo"
+                :credentials "yes"
+                :cheese "lol"))
