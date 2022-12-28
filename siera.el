35c8de04c773988502d10ed921d0fdbd3e31d4a3
blu3beri <blu3beri@proton.me>
2022-12-28 18:39:55 +0100

    Finished macro setup

--- siera.el
+++ siera.el
@@ -2,40 +2,28 @@
   (or delim (setq delim " "))
   (string-join args " "))
 
-(mapatoms (lambda (symbol)
-            (if (string-prefix-p "siera-" (symbol-name symbol))
-                (unintern symbol))))
-
 (defun siera--command (command)
   "Wrapper for siera"
   (setq siera (executable-find "siera"))
   (if (not siera)
       (error "siera is not installed"))
-  (setq formatted-command (format "%s %s 2> /dev/null" siera (string-join-with-delim command))
+  (setq formatted-command
+        (s-replace ")" "" (s-replace "(" "" (format "%s %s 2> /dev/null" siera command)))
         command-output (shell-command-to-string formatted-command)
         out command-output)
   (message out))
 
-(defun siera-plist! (plist)
-  (dolist (command (list :connection :proof :credential))
-    (setq subcommands (plist-get plist command))
-    (setq command (substring (format "%s" command) 1))
-    (when (boundp 'subcommands)
-      (or (listp subcommands) (setq subcommands (list subcommands)))
-      (dolist (subcommand subcommands)
-        (setq subcommand (format "%s" subcommand))
-        `(defun (intern (format "siera--%s-%s" command subcommand)) ()
-             ,(format "Siera subcommand ey: %s - %s" command subcommand)
-             (message "kaas")
-             ;;(siera--command (command subcommand))
-             )))))
+(defmacro siera! (&rest commands)
+  (macroexp-progn
+   (mapcar (lambda (command)
+           `(defun ,(intern (format "siera--%s" command)) (&optional args)
+              ,(format "Siera wrapper for %s subcommand" command)
+              (setq a (append (list ,(format "%s" command) args)))
+              (siera--command a))) commands)))
 
-(siera-plist! '(:connection
-               (invite list)
-               :proof
-               foo
-               :credential
-               yes
-               ))
+(siera! connection
+        proof
+        credential)
 
-(siera--connection-invite)
+;;(siera--connection '(invite --qr))
+;;(siera--credential '(-h))
